db8f8cc0b68508571a2d35cf275df2c233dfa414
Barbara Mingatos <72284498+babimingatos@users.noreply.github.com>
2023-03-11 21:50:02 -0300

    Update Ex_02.java

--- POO/Lista 0/Ex_02.java
+++ POO/Lista 0/Ex_02.java
@@ -1,1 +1,35 @@
+package lista0;
+
+import java.util.Scanner;
+
+public class Ex_02 {
+
+	public static void main(String[] args) {
+		// Barbara Mingatos
+
+		Scanner sc = new Scanner(System.in);
+
+		int[][] array = new int[6][3];
+
+		System.out.print("Original");
+		int i, j = 0;
+		for (i = 0; i < 6; i++) {
+			System.out.println();
+
+			for (j = 0; j < 3; j++) {
+				array[i][j] = (int) (Math.random() * 8);
+				System.out.print(array[i][j] + " ");
+			}
+		}
+
+		System.out.print("\n\nInvertida");
+		for (int l = array.length - 1; l >= 0; l--) {
+			System.out.println();
+			for (int m = array[l].length - 1; m >= 0; m--) {
+				System.out.print(" " + array[l][m]);
+			}
+		}
+	}
+
+}
 
