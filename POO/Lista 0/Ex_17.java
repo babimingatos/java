d7514a9db4237e4b0b57cccbf163a35aa6c2012c
Barbara Mingatos <72284498+babimingatos@users.noreply.github.com>
2023-03-16 13:18:19 -0300

    Create Ex_17.java

---
+++ POO/Lista 0/Ex_17.java
@@ -0,0 +1,35 @@
+package lista0;
+
+import java.util.Scanner;
+
+public class Ex_17 {
+
+	public static void main(String[] args) {
+		// Barbara Mingatos
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Digite a dimensão: ");
+		int dim = sc.nextInt();
+
+		int[][] matriz = new int[dim][dim];
+
+		for (int i = 0; i < dim; i++) {
+			System.out.println();
+			for (int j = 0; j < dim; j++) {
+				matriz[i][j] = (int) (Math.random() * 8) + 1;
+				System.out.print(" " + matriz[i][j]);
+			}
+		}
+
+		System.out.print("\n\nDiagonal: ");
+
+		for (int i = 0; i < dim; i++) {
+			for (int j = 0; j < dim; j++) {
+				if (j >= i && j + i >= dim - 1) {
+					System.out.print(matriz[i][j] + " ");
+				}
+			}
+		}
+	}
+}
