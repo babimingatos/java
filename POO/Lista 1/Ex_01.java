3c0f2e86b68a8e056eaa10e9587de55e211d6111
Barbara Mingatos <72284498+babimingatos@users.noreply.github.com>
2023-03-28 20:31:04 -0300

    Update Ex_01.java

--- POO/Lista 1/Ex_01.java
+++ POO/Lista 1/Ex_01.java
@@ -1,1 +1,29 @@
 
+package lista1;
+
+import java.util.Scanner;
+
+public class Ex_01 {
+	// Barbara Mingatos
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Digite um número: ");
+		double num = sc.nextDouble();
+
+		if (positivo(num)) {
+			System.out.print(num + " é positivo");
+		} else {
+			System.out.print(num + " é negativo");
+		}
+	}
+
+	static boolean positivo(double num) {
+		if (num >= 0) {
+			return true;
+		}
+		return false;
+
+	}
+}
