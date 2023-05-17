f27bec96bcab48d78b26397c57f923795c9a1706
Barbara Mingatos <72284498+babimingatos@users.noreply.github.com>
2023-05-17 17:47:43 -0300

    Create Elevador.java

---
+++ POO/Exercícios em Sala/Elevador/Elevador.java
@@ -0,0 +1,8 @@
+- Contar quantos andares tem
+- Qauntas pessoas entram e qual periodo ela entrou e saiu
+- Quantas pessoas saem e qual periodo ela entrou e saiu
+- Contar máximo de pessoas
+- Com 0 pessoas ele para, não movimenta
+- construtor q faz para o predio ir para o subterraneo
+- inferior é zero
+- 
