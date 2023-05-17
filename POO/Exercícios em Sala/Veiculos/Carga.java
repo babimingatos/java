c49bd3bd264a14a55807d70123774140ea265224
Barbara Mingatos <72284498+babimingatos@users.noreply.github.com>
2023-05-17 18:20:48 -0300

    Create Carga.java

---
+++ POO/Exercícios em Sala/Veiculos/Carga.java
@@ -0,0 +1,19 @@
+package veiculos;
+
+public class Carga extends Veiculos {
+
+    private int capacidade;
+
+    public Carga() {
+
+    }
+
+    public void setCapacidade(int capacidade) {
+        this.capacidade = capacidade;
+    }
+
+    public int getCapacidade() {
+        return capacidade;
+    }
+
+}
