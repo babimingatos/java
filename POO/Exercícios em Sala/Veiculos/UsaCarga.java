e9799ebc98eaee13e6beef3fb9301128379d4ef5
Barbara Mingatos <72284498+babimingatos@users.noreply.github.com>
2023-05-17 18:26:33 -0300

    Create UsaCarga.java

---
+++ POO/Exercícios em Sala/Veiculos/UsaCarga.java
@@ -0,0 +1,17 @@
+
+package veiculos;
+import java.util.Scanner;
+public class UsaCarga {
+    public static void main(String[]args){
+    
+    Scanner sc=new Scanner(System.in);
+    
+    Carga carga=new Carga();
+    
+    System.out.print("Capacidade: "+carga.getCapacidade());
+     System.out.print("Valor KM inicial: "+veiculos.getKminicial);
+    
+    }
+    
+    
+}
