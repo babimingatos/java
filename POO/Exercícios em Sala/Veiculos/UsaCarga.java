d2b13012d4693bcb65b3caadc997d940c7b10b0c
Barbara Mingatos <72284498+babimingatos@users.noreply.github.com>
2023-05-17 18:27:21 -0300

    Update UsaCarga.java

--- POO/Exercícios em Sala/Veiculos/UsaCarga.java
+++ POO/Exercícios em Sala/Veiculos/UsaCarga.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 public class UsaCarga {
     public static void main(String[]args){
     
-    Scanner sc=new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
     
-    Carga carga=new Carga();
+    Carga carga = new Carga();
     
-    System.out.print("Capacidade: "+carga.getCapacidade());
-     System.out.print("Valor KM inicial: "+veiculos.getKminicial);
+    System.out.print("Capacidade: " + carga.getCapacidade());
+     System.out.print("Valor KM inicial: " + veiculos.getKminicial);
     
     }
     
